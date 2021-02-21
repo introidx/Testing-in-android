89cf34d91bc3e75473de25c04fa7ccf4db53d8c5
prakashRanjan19 <prakash.r19500@gmail.com>
2021-02-21 15:38:25 +0530

    Before and after

---
+++ app/src/androidTest/java/com/example/testinginandroid/ResourceComparerTest.kt
@@ -0,0 +1,37 @@
+package com.example.testinginandroid
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+
+class ResourceComparerTest{
+
+    // create an instance of ResourceComparer class
+    private lateinit var  resourceComparer : ResourceComparer
+
+    // by using @Before ..it will execute this method first
+    // we can also initialize ResourceComparer object in each of the methods but that will create many boilerplate code
+    // so annotate it with @Before in order to execute it first
+    @Before
+    fun setUp(){
+        resourceComparer = ResourceComparer()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name , "TestingInAndroid")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context , R.string.app_name , "Hello")
+        assertThat(result).isFalse()
+    }
+    
+}
